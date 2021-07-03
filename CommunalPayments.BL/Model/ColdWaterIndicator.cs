7c2e82e0df125d920928dc694a50bbc57567466f
Neverus93 <svsilent2054@gmail.com>
2021-07-03 19:08:24 +0300

    Created Indicators classes

---
+++ CommunalPayments.BL/Model/ColdWaterIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CommunalPayments.BL.Model
+{
+    /// <summary>
+    /// Показатель счётчика холодной воды
+    /// </summary>
+    [Serializable]
+    class ColdWaterIndicator
+    {
+        /// <summary>
+        /// Показатель счётчика холодной воды
+        /// </summary>
+        public double ColdWaterIndicatorValue { get; set; }
+
+        /// <summary>
+        /// Создать новое показание счётчика холодной воды
+        /// </summary>
+        /// <param name="coldWaterIndicatorValue">Показатель счётчика холодной воды</param>
+        public ColdWaterIndicator(double coldWaterIndicatorValue)
+        {
+            #region Проверка условий
+            if (coldWaterIndicatorValue < 0)
+            {
+                throw new ArgumentException("Значение показателя счётчика холодной воды не может быть отрицательным числом");
+            }
+            #endregion
+
+            ColdWaterIndicatorValue = coldWaterIndicatorValue;
+        }
+    }
+}
