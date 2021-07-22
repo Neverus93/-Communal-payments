dbedaad9dbe660240d32900976b180ec734cfbde
Neverus93 <svsilent2054@gmail.com>
2021-07-22 21:41:59 +0300

    Rework classes

---
+++ CommunalPayments.BL/Model/DataIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments.BL.Model
+{
+    [Serializable]
+    class DataIndicator
+    {
+        public double ColdWater { get; set; }
+        public double HotWater { get; set; }
+        public double Electricity { get; set; }
+
+        public DataIndicator()
+        {
+
+        }
+
+        public DataIndicator(double coldWater, double hotWater, double electricity)
+        {
+            ColdWater = coldWater;
+            HotWater = hotWater;
+            Electricity = electricity;
+        }
+    }
+}
