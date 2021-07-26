344811069860c4b6f426740b91f41f14603211a5
Sergey Romanov <sergey.romanov@isbc.com>
2021-07-26 12:37:41 +0300

    Добавлено сохранение введённых данных в файл(пока без условий и тестов)

--- CommunalPayments.MainWindow/DataIndicator.cs
+++ CommunalPayments.MainWindow/DataIndicator.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
-namespace CommunalPayments.BL.Model
+namespace CommunalPayments.MainWindow
 {
     [Serializable]
-    class DataIndicator
+    public class DataIndicator
     {
         public double ColdWater { get; set; }
         public double HotWater { get; set; }
--- CommunalPayments.MainWindow/MainWindow.xaml.cs
+++ CommunalPayments.MainWindow/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
 
 namespace CommunalPayments.MainWindow
 {
@@ -10,6 +12,26 @@ namespace CommunalPayments.MainWindow
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SaveCalling_Click(object sender, RoutedEventArgs e)
+        {
+            double coldWater = double.Parse(ColdWaterInput.Text);
+            double hotWater = double.Parse(HotWaterInput.Text);
+            double electricity = double.Parse(ElectricityInput.Text);
+
+            SaveData(coldWater, hotWater, electricity);
+        }
+
+        public void SaveData(double cold, double hot, double electro)
+        {
+            DataIndicator data = new DataIndicator(cold, hot, electro);
+            XmlSerializer formatter = new XmlSerializer(typeof(DataIndicator));
+
+            using (FileStream fs = new FileStream("DataIndicator.xml", FileMode.Append, FileAccess.Write))
+            {
+                formatter.Serialize(fs, data);
+            }
         }
     }
 }
