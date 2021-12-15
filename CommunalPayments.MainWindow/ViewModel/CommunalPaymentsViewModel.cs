9048147b7e8b585fd2e928d33e42879eb1160d7d
Sergey Romanov <romanov.s@isbc.ru>
2021-12-15 16:13:08 +0300

    Add MVVM scheme for continue changes

---
+++ CommunalPayments.MainWindow/ViewModel/CommunalPaymentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalPayments.MainWindow.ViewModel
+{
+    class CommunalPaymentsViewModel
+    {
+        //Будет содержать все основные биндинги и команду сохранения данных показателей
+    }
+}
