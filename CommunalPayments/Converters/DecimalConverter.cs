4e1874d0ad3909e642b6877b27624758fb2faa1d
Sergey Romanov <romanov.s@isbc.ru>
2023-12-05 13:41:53 +0300

    Реализовано обнуление TextBox.Text при очищении формы

---
+++ CommunalPayments/Converters/DecimalConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CommunalPayments.Converters
+{
+    internal class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!decimal.TryParse(value.ToString(), out decimal d))
+            {
+                d = 0;
+            }
+            return d;
+        }
+    }
+}
